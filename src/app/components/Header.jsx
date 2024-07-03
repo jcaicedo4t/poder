205d202e5d7616121d8e2bbe57bbab079844d3d8
Jose Caicedo <josec@ninoprodigio.net>
2024-07-03 15:10:14 -0500

    Login

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -54,7 +54,7 @@ export default function Header() {
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link href="#">
+          <Link href="/login">
             <span className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </span>
@@ -99,7 +99,7 @@ export default function Header() {
               </div>
 
               <div className="py-6">
-                <Link href="#">
+                <Link href="/login">
                   <span className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Log in
                   </span>
