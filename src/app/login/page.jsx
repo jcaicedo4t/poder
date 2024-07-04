782085d8370713511454307673714813eb757eb7
Jose Caicedo <josec@ninoprodigio.net>
2024-07-04 16:27:19 -0500

    Changes

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -13,8 +13,8 @@ export default function LoginPage() {
   return (
     <>
       {!session ? (
-        <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-<div className="sm:mx-auto sm:w-full sm:max-w-md w-[24rem]">
+        <div className="flex min-h-full flex-1 flex-col justify-center sm:py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md w-[24rem]">
 <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Iniciar sesión en su cuenta
             </h2>
