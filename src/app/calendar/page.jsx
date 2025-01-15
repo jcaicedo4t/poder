42543ddc0128af100de98838f81dfda5a629e880
Jose Caicedo <josec@ninoprodigio.net>
2025-01-15 15:34:42 -0500

    Cambioss

--- src/app/calendar/page.jsx
+++ src/app/calendar/page.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+// "use client";
 
-export default function page() {
+// import React, { useState } from "react";
+import Calendar from "../calendar/HomeCalendar";
+import CalendarEvents from "../calendar/CalendarEvent";
+
+export default function Example() {
+  // const [reloadCalendar, setReloadCalendar] = useState(false);
+
+  // const onCreateEvent = (newEvent) => {
+  //   console.log("Evento creado:", newEvent);
+
+  //   // Opcional: Aquí puedes hacer cualquier lógica adicional con `newEvent`
+
+  //   setReloadCalendar(true); // Activa la recarga del calendario
+  // };
+
+  // const onCalendarReloaded = () => {
+  //   setReloadCalendar(false); // Resetea el estado después de que el calendario se recargue
+  // };
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <h2 className="text-base font-semibold text-gray-900 mb-5">
+        Próximos eventos
+      </h2>
+      <div className="lg:grid ">
+        <CalendarEvents/>
+        <div className="mr-24 w-[40rem]">
+          <Calendar  />
+        </div>
+      </div>
+    </div>
+  );
 }
