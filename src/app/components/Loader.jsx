3053d6009225ff05d7acc30e139d7a9a35eb694d
Jose Caicedo <josec@ninoprodigio.net>
2025-01-16 09:55:08 -0500

    Cambios de implementar loading login y middleware

---
+++ src/app/components/Loader.jsx
@@ -0,0 +1,34 @@
+export default function Loader() {
+    return (
+      <div className="loader-container">
+        <div className="loader"></div>
+        <style jsx>{`
+          .loader-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            margin: 0;
+          }
+          .loader {
+            width: 60px;
+            aspect-ratio: 2;
+            --_g: no-repeat radial-gradient(circle closest-side, #000 90%, #0000);
+            background: 
+              var(--_g) 0%   50%,
+              var(--_g) 50%  50%,
+              var(--_g) 100% 50%;
+            background-size: calc(100% / 3) 50%;
+            animation: l3 1s infinite linear;
+          }
+          @keyframes l3 {
+            20% { background-position: 0%   0%, 50%  50%, 100%  50% }
+            40% { background-position: 0% 100%, 50%   0%, 100%  50% }
+            60% { background-position: 0%  50%, 50% 100%, 100%   0% }
+            80% { background-position: 0%  50%, 50%  50%, 100% 100% }
+          }
+        `}</style>
+      </div>
+    );
+  }
+  
